7a6266a98d14f120e30201678cfbee4a9a537f55
Coding rev <fernando.lobo.prez@gmail.com>
2025-06-28 03:36:20 -0300

    02-Using transitions
    02-Using transitions

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,29 @@
 <script>
+  import { fade, blur, slide, scale } from "svelte/transition";
+  let numbers = $state([]);
+
+  function addNumbers(){
+    numbers.push(Math.floor(Math.random()*100));
+    numbers = numbers
+  }
 
 </script>
 
-<h1>Transitions</h1>
+<div class="container">
+  <h1>Numbers</h1>
+  <div>
+    {#each numbers as number,i(number) }
+      <button class="element" transition:scale>{number}</button>
+    {/each}
+  </div>
+  <button class="add" onclick={addNumbers}>Add number</button>
+
+</div>
+
+<style>
+  .element {
+    width:100%;
+    padding: 10px;
+    margin:5px
+  }
+</style>
